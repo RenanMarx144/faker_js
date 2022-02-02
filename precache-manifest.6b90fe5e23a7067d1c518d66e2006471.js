3b403df363d0032a075ff2765a11b66d59ff75d5
RenanMarx <renan@ziriga.com.br>
2022-02-01 23:03:16 -0300

    ct

---
+++ precache-manifest.6b90fe5e23a7067d1c518d66e2006471.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002520a4078d9738f4a9",
+    "url": "/css/app.89040590.css"
+  },
+  {
+    "revision": "9dec36bde46b15d37184",
+    "url": "/css/chunk-vendors.b327e5bf.css"
+  },
+  {
+    "revision": "6868a14e2aa49eb7e089c13233fcaa86",
+    "url": "/img/camisa.6868a14e.jpg"
+  },
+  {
+    "revision": "a80cc36ebd235695b7d88ad6aab21969",
+    "url": "/img/caneca.a80cc36e.jpg"
+  },
+  {
+    "revision": "30a738a1a3527baacd8c58002752564f",
+    "url": "/img/capa.30a738a1.jpg"
+  },
+  {
+    "revision": "a9e4dcb196ff908f6cc5a48b61dfac83",
+    "url": "/img/img1.a9e4dcb1.jpg"
+  },
+  {
+    "revision": "cbfbf47ba85a9d317c3e1a19f192e83f",
+    "url": "/img/spinner.cbfbf47b.gif"
+  },
+  {
+    "revision": "e49012a4eeb921fa069c6ae2132cf19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e8d040e8e1e591cff98",
+    "url": "/js/about.367cd35c.js"
+  },
+  {
+    "revision": "002520a4078d9738f4a9",
+    "url": "/js/app.3b2e90e8.js"
+  },
+  {
+    "revision": "9dec36bde46b15d37184",
+    "url": "/js/chunk-vendors.e8ac5919.js"
+  },
+  {
+    "revision": "73cf5e0c3a2981a2193a7d015bfeafbd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
